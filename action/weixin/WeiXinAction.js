8b34ce80e18aecf2daa900b1270d231544b741ec
cloudbian <cloudbian@gmail.com>
2014-04-24 10:24:40 +0800

    update weixin action

--- action/weixin/WeiXinAction.js
+++ action/weixin/WeiXinAction.js
@@ -3,17 +3,17 @@
  */
 var weixin = require('./../../tools/WeiXin');
 exports.notify = function(req,res){
-    var signature = req.query.signature;
-    var ts = req.query.timestamp;
-    var nonce = req.query.nonce;
+//    var signature = req.query.signature;
+//    var ts = req.query.timestamp;
+//    var nonce = req.query.nonce;
     var echostr = req.query.echostr;
-    if(weixin.check(signature,ts,nonce,echostr)){
-        if(null==echostr){
-
-        }else{
+//    if(weixin.check(signature,ts,nonce,echostr)){
+//        if(null==echostr){
+//
+//        }else{
             res.send(echostr);
-        }
-    }else{
-        res.send('error');
-    }
+//        }
+//    }else{
+//        res.send('error');
+//    }
 };
