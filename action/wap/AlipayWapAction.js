95968dad85472b99edf69cfa3362fc379f770808
cloudbian <cloudbian@gmail.com>
2014-04-25 17:51:10 +0800

    update wap subOrder and confirmOrder

--- action/wap/AlipayWapAction.js
+++ action/wap/AlipayWapAction.js
@@ -2,20 +2,39 @@
  * Created by cloudbian on 14-4-3.
  */
 var alipay = require('./../../tools/Alipay.js');
-
+var Config = require('./../../tools/Config.js');
+var HttpClient = require('./../../tools/HttpClient.js');
 //请求交易
 exports.getReqTrade = function(req,res){
     //todo 参数取值
-    var tradeNo = 100038;
-    var subject = 'test';
+    var _id = req.body._id;
+    var tradeNo = req.body.oid;
     var total_fee = "0.01";
-    alipay.wap.reqAuth(tradeNo,subject,total_fee,function(error,token){
-        if(""===token){
-            res.send(404,'token is null');
-        }else{
-            res.send(alipay.wap.reqTrade(token));
-        }
+    var httpClient = new HttpClient({
+        'host':Config.inf.host,
+        'port':Config.inf.port,
+        'path':'/order/detail/'+_id,
+        'method':"GET"
     });
+    try{
+        httpClient.getReq(function(err,result){
+            if(err){
+                res.send(404,"error is "+err);
+            }else{
+                var subject = result.data.product.name;
+//                total_fee = result.data.totalPrice+"";
+                alipay.wap.reqAuth(tradeNo,subject,total_fee,function(error,token){
+                    if(""===token){
+                        res.send(404,'token is null');
+                    }else{
+                        res.send(alipay.wap.reqTrade(token));
+                    }
+                });
+            }
+        });
+    }catch(e){
+        res.send(404,"error is "+ e.message);
+    }
 }
 
 //同步回调
--- action/wap/ProductPageAction.js
+++ action/wap/ProductPageAction.js
@@ -4,7 +4,7 @@
 var HttpClient = require('./../../tools/HttpClient.js');
 var Config = require('./../../tools/Config');
 var async = require('async');
-
+var timeZone = ' 00:00:00 +08:00';
 exports.getProducts = function(request,response){
     var city = request.params.id;
     async.series([
@@ -60,6 +60,7 @@ exports.getDetail = function(request,response){
             if(0===res.error){
                 console.log(res.data);
                 var product = res.data;
+                var level = "";
                 if(product.type==1){
                     switch(product.level){
                         case 1:
@@ -137,4 +138,138 @@ exports.toSubOrder = function(request,response){
             }
         }
     });
+}
+
+exports.saveOrder = function(request,response){
+    var us = require('underscore');
+    var data = {};
+    var flag = true;
+    var errorMsg = "";
+    var id = "";
+    data.source = "534de2e3309199c11f233cf4";
+    data.payWay = "1";
+    data.remark = "";
+    if(!us.isEmpty(request.session.user)){
+        data.member = request.session.user._id;
+    }else{
+        errorMsg = "请先登录";
+        flag = false;
+    }
+    if(!us.isEmpty(request.body.pid)){
+        data.product = request.body.pid;
+    }else{
+        errorMsg = "产品ID为空";
+        flag = false;
+    }
+    if(!us.isEmpty(request.body.type)){
+        var orderType = request.body.type;
+        if(4===parseInt(orderType)){
+            data.startDate = new Date(request.body.time+timeZone).getTime();
+        }else{
+            var arrays = request.body.time.split('~');
+            data.startDate = new Date(arrays[0]+timeZone).getTime();
+            data.endDate = new Date(arrays[1]+timeZone).getTime();
+            if(!us.isEmpty(request.body.isWeekend)){
+                data.priceLogisWeeknd = "y"===request.body.isWeekend?true:false;
+            }else{
+                errorMsg = "是否周末不能为空";
+                flag = false;
+            }
+            if(!us.isEmpty(request.body.lid)){
+                data.priceLog = request.body.lid;
+            }else{
+                errorMsg = "是否周末不能为空";
+                flag = false;
+            }
+        }
+    }else{
+        errorMsg = "订单类型为空";
+        flag = false;
+    }
+    if(!us.isEmpty(request.body.num)){
+        data.quantity = request.body.num;
+    }else{
+        errorMsg = "产品数量不能为空或为0";
+        flag = false;
+    }
+    if(!us.isEmpty(request.body.name)){
+        data.liveName = request.body.name;
+    }else{
+        errorMsg = "姓名不能为空";
+        flag = false;
+    }
+    if(!us.isEmpty(request.body.mobile)){
+        data.contactPhone = request.body.mobile;
+    }else{
+        errorMsg = "联系手机号不能为空";
+        flag = false;
+    }
+    if(!us.isEmpty(request.body.isNeed)&&"y"===request.body.isNeed){
+        if(!us.isEmpty(request.body.inType)){
+            data.invoiceType = request.body.inType;
+        }else{
+            errorMsg = "发票类型不能为空";
+            flag = false;
+        }
+        if(!us.isEmpty(request.body.title)){
+            data.invoiceTitle = request.body.title;
+        }else{
+            errorMsg = "发票抬头不能为空";
+            flag = false;
+        }
+        if(!us.isEmpty(request.body.address)){
+            data.invoiceAdd = request.body.address;
+        }else{
+            errorMsg = "发票地址不能为空";
+            flag = false;
+        }
+    }
+    if(flag){
+        var httpClient = new HttpClient({
+            'host':Config.inf.host,
+            'port':Config.inf.port,
+            'path':'/order/save',
+            'method':"POST"
+        });
+        httpClient.postReq(data,function(err,result){
+            if(err){
+                response.send(404,err);
+            }else{
+                if(0===result.error){
+                    var res = {};
+                    res.name = request.body.pName;
+                    res.orderType = request.body.type;
+                    res._id = result.data._id;
+                    res.oid = result.data.orderID;
+                    if(4===parseInt(request.body.type)){
+                        res.time = new Date(result.data.startDate).Format("yyyy-MM-dd");
+                    }else{
+                        var sd = new Date(result.data.startDate).Format("yyyy-MM-dd");
+                        var ed = new Date(result.data.endDate).Format("yyyy-MM-dd");
+                        res.time = sd + '~' + ed;
+                    }
+                    res.num = result.data.quantity;
+                    res.total = result.data.totalPrice;
+                    res.person = result.data.liveName;
+                    res.mobile = result.data.contactPhone;
+                    if(!us.isEmpty(result.data.invoice.types)){
+                        res.isNeed = "y";
+                        res.invoiceType = result.data.invoice.types;
+                        res.invoiceTitle = result.data.invoice.title;
+                        res.invoiceAdd = result.data.invoice.address;
+                    }else{
+                        res.isNeed = "n";
+                    }
+                    response.render('wap/orderConfirm',{titleName:'支付订单',info:res});
+                }else{
+                    console.log(result.errorMsg);
+                    response.send(404,result.errorMsg);
+                }
+            }
+        });
+
+    }else{
+        console.log(errorMsg);
+        response.send(404,errorMsg);
+    }
 }
