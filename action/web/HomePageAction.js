6b8f407f1d7b34640a3296143bbaf93ffa3133c3
wuchong <chong.wu.xiangfan@gmail.com>
2014-04-28 14:22:23 +0800

    首页、产品详情页修改

--- action/web/HomePageAction.js
+++ action/web/HomePageAction.js
@@ -3,6 +3,7 @@
  */
 var HttpClient = require('./../../tools/HttpClient.js');
 var Config = require('./../../tools/Config');
+var _ = require('underscore');
 
 exports.cityBox = function(request,response){
     var httpClient = new HttpClient({
@@ -13,7 +14,7 @@ exports.cityBox = function(request,response){
     });
     httpClient.getReq(function(err,res){
         if(err){
-            response.send(404,'fuck 404');
+            response.send(404,'404');
         } else {
             response.render('web/template/cityBox',{'cities':res.data});
         }
@@ -24,6 +25,41 @@ exports.home = function(request,response){
     response.render('web/home');
 };
 
+var truncProductName = function(productName){
+    return productName.length>7?productName.substr(0,7)+'...':productName;
+};
+
+exports.hotProduct = function(req,res){
+    try{
+        var city  = req.query.cityID;
+        if(_.isEmpty(city)){
+            city='5343757bd8d3efb068465b1f';
+        }
+        var httpClient = new HttpClient({
+            'host':Config.inf.host,
+            'port':Config.inf.port,
+            'path':'/web/product/webList/'+city+'?isHot=true',
+            'method':"GET"
+        });
+        console.log('/web/product/webList/'+city+'isHot=true');
+        httpClient.getReq(function(err,data){
+            if(err){
+                res.json({error:1,errorMsg: err});
+            } else {
+                console.log(data.data);
+                //只取第一张图片(这个逻辑可以更改)
+                data.data.forEach(function(data){
+                    data.shortName = truncProductName(data.name);
+                    data.image = data.image[0];
+                    data.image.url=   'http://dd885.b0.upaiyun.com/'+data.image.url+"!hotProductList";
+                });
+                res.json(data);
+            }
+        });
+    }catch(e){
+        res.json({error:1,errorMsg: e.message});
+    }
+}
 
 //exports.hotProduct = function(request,response){
 //    var city = request.query.city;
